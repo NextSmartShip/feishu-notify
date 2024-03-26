1c8a336d77417924213d16c6970c5633bbe021fe
jawQ <757050031@qq.com>
2024-03-26 17:49:49 +0800

    feat🎉: 适配eslint ````

--- dist/index.js
+++ dist/index.js
@@ -29035,28 +29035,51 @@ exports.run = run;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
+const core = __importStar(__nccwpck_require__(2186));
 const github_1 = __importDefault(__nccwpck_require__(5438));
 // import { PushEvent } from '@octokit/webhooks-definitions/schema';
 // import type { UserDefinedOptions } from '../type'
 const getActionOptions = () => {
-    const token = core_1.default.getInput('token');
-    const username = core_1.default.getInput('username');
+    const token = core.getInput('token');
+    const username = core.getInput('username');
     // getBooleanInput 其实本质上就是一种 parseBoolean(core.getInput('key'))
-    const motto = core_1.default.getBooleanInput('motto');
-    const filepath = core_1.default.getInput('filepath');
-    const title = core_1.default.getInput('title');
-    const includeFork = core_1.default.getBooleanInput('includeFork');
-    const includeArchived = core_1.default.getBooleanInput('includeArchived');
-    const onlyPrivate = core_1.default.getBooleanInput('onlyPrivate');
-    core_1.default.info(`当前事件：${github_1.default.context.eventName}`);
+    const motto = core.getBooleanInput('motto');
+    const filepath = core.getInput('filepath');
+    const title = core.getInput('title');
+    const includeFork = core.getBooleanInput('includeFork');
+    const includeArchived = core.getBooleanInput('includeArchived');
+    const onlyPrivate = core.getBooleanInput('onlyPrivate');
+    core.info(`当前事件：${github_1.default.context.eventName}`);
     if (github_1.default.context.eventName === 'push') {
         const pushPayload = github_1.default.context.payload;
-        core_1.default.info(`The head commit is: ${pushPayload.head_commit}`);
+        core.info(`The head commit is: ${pushPayload.head_commit}`);
     }
     return {
         token,
