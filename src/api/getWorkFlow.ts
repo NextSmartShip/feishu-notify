6391401d40e6704dfb2a13c49b2f5255eb641468
jawQ <757050031@qq.com>
2024-03-29 13:22:07 +0800

    config📦: 完善action

---
+++ src/api/getWorkFlow.ts
@@ -0,0 +1,33 @@
+import axios from './request'
+import push from './push'
+import { stop } from '../utils'
+import { fetchWorkFlow } from '.'
+
+interface Props {
+  owner?: string
+  repo?: string
+  run_id?: number
+}
+const getWorkFlow = async ({
+  owner = 'NextSmartShip',
+  repo = '',
+  run_id = -1,
+  ...props
+}: Props) => {
+  if (!repo || run_id === -1)
+    throw new Error('参数丢失，请检查repo和run_id是否同时传入')
+
+  try {
+    await stop(3000)
+    const payload = await fetchWorkFlow({
+      owner,
+      repo,
+      run_id
+    })
+    await push(payload)
+  } catch (error) {
+    console.log('查看请求by错误时：', error)
+  }
+}
+
+export default getWorkFlow
